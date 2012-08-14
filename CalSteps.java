511d1f6012c59e9283d19cc2147b96c8fc79b9d4
Dillon Nichols <djn5065@psu.edu>
2012-08-13 20:28:21 -0400

    working version of desktop GUI

--- CalSteps.java
+++ CalSteps.java
@@ -1,45 +1,21 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
-import java.util.*;
+import java.lang.String.*;
 
 public class CalSteps extends JFrame implements ActionListener {
 	
-	// Set up row 1
-	JPanel row1 = new JPanel();
-	// Adds text and room for input
-	JLabel oldStepsText = new JLabel("Old steps-per-mm value: ", JLabel.RIGHT);
+	// Sets up field variables to use in calculate()
 	JTextField oldSteps = new JTextField(12);
-		
-	// Set up row 2
-	JPanel row2 = new JPanel();
-	// Adds text and room for input
-	JLabel definedLengthText = new JLabel("Defined object length: ", JLabel.RIGHT);
 	JTextField definedLength = new JTextField(12);
-		
-	// Set up row 3
-	JPanel row3 = new JPanel();
-	// Adds text and room for input
-	JLabel measuredLengthText = new JLabel("Measured object length: ", JLabel.RIGHT);
 	JTextField measuredLength = new JTextField(12);
-
-	// Set up row 4
-	JPanel row4 = new JPanel();
-	// Adds button to begin calculation
-	JButton calculateButton = new JButton("Calculate");
-	calculateButton.addActionListener(this);
-
-	// Set up row 5
-	JPanel row5 = new JPanel();
-	// Adds text and room for output
-	JLabel newStepsText = new JLabel("New steps-per-mm value: ", JLabel.RIGHT);
 	JTextField newSteps = new JTextField(12);
-		
+						
 	public CalSteps() {
 		// Sets up defaults for UI
 		super("3D Printer Steps-per-mm Calculator");
 		setLookAndFeel();
-		setSize(600,400);
+		setSize(400,300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// Sets up layout for entire app
 		FlowLayout flow = new FlowLayout();
@@ -47,7 +23,8 @@ public class CalSteps extends JFrame implements ActionListener {
 		
 		// Sets up layout for row 1
 		FlowLayout layout1 = new FlowLayout(FlowLayout.CENTER, 10, 10);
-		// Adds the components of row 1 using layout1
+		JPanel row1 = new JPanel();
+		JLabel oldStepsText = new JLabel("Old steps-per-mm value: ", JLabel.RIGHT);
 		row1.setLayout(layout1);
 		row1.add(oldStepsText);
 		row1.add(oldSteps);
@@ -55,7 +32,8 @@ public class CalSteps extends JFrame implements ActionListener {
 		
 		// Sets up layout for row 2
 		FlowLayout layout2 = new FlowLayout(FlowLayout.CENTER, 10, 10);
-		// Adds the components of row 2 using layout2
+		JPanel row2 = new JPanel();
+		JLabel definedLengthText = new JLabel("Defined object length: ", JLabel.RIGHT);
 		row2.setLayout(layout2);
 		row2.add(definedLengthText);
 		row2.add(definedLength);
@@ -63,22 +41,27 @@ public class CalSteps extends JFrame implements ActionListener {
 
 		// Sets up layout for row 3
 		FlowLayout layout3 = new FlowLayout(FlowLayout.CENTER, 10, 10);
-		// Adds the components of row 3 using layout3
+		JPanel row3 = new JPanel();
+		JLabel measuredLengthText = new JLabel("Measured object length: ", JLabel.RIGHT);
 		row3.setLayout(layout3);
 		row3.add(measuredLengthText);
 		row3.add(measuredLength);
 		add(row3);
 
 		// Sets up layout for row 4
-		FlowLayout layout4 = new FlowLayout(FlowLayout.CENTER, 10, 10);
-		// Adds the components of row 4 using layout4
+		FlowLayout layout4 = new FlowLayout(FlowLayout.CENTER, 10, 10);     
+		JPanel row4 = new JPanel();
+		// Adds button to begin calculation
+		JButton calculateButton = new JButton("Calculate");
+		calculateButton.addActionListener(this);
 		row4.setLayout(layout4);
 		row4.add(calculateButton);
 		add(row4);
 
 		// Sets up layout for row 5
 		FlowLayout layout5 = new FlowLayout(FlowLayout.CENTER, 10, 10);
-		// Adds the components of row 5 using layout5
+		JPanel row5 = new JPanel();
+		JLabel newStepsText = new JLabel("New steps-per-mm value: ", JLabel.RIGHT);
 		row5.setLayout(layout5);
 		row5.add(newStepsText);
 		row5.add(newSteps);
@@ -96,17 +79,19 @@ public class CalSteps extends JFrame implements ActionListener {
 	}
 
 	void calculate() {
-		float oldStepsValue = Float.parseFloat(field.oldSteps);
-		float definedLengthValue = Float.parseFloat(field.definedLength);
-		float measuredLengthValue = Float.parseFloat(field.measuredLength);
+		float oldStepsValue = Float.parseFloat(oldSteps.getText());
+		float definedLengthValue = Float.parseFloat(definedLength.getText());
+		float measuredLengthValue = Float.parseFloat(measuredLength.getText());
 		float newStepsValue = ((oldStepsValue * definedLengthValue) / measuredLengthValue);
-		field.newSteps.setText(newStepsValue);
+		String newStepsValueString;
+		newStepsValueString = String.valueOf(newStepsValue);
+		newSteps.setText(newStepsValueString);
 	}
 
 	private void setLookAndFeel() {
 		try {
 			UIManager.setLookAndFeel(
-					"com.sun.java.swing.plaf.nimbus.NumbusLookAndFeel"
+				"com.sun.java.swing.plaf.nimbus.NumbusLookAndFeel"
 			);
 		} catch (Exception exc) {
 			// ignore error
